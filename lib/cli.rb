940bfd6e95c363f49eed47dd403ecfb0a6eae086
igallardo89 <igallardo0716@gmail.com>
2020-10-10 12:17:35 -0400

    message

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,12 +14,28 @@ class CLI
         leagues = @competitions.map{ |competition| competition["competition"]["name"] }.map { |name|  name.match(/(^.*):/)[1] }.uniq
         leagues.each do |league|
         puts "#{league}"
-      
       end
-    end
+        puts " "
+        puts "Please a Country or Global League for games, or input 'exit' to quit."
+        puts " "
+        inp = gets.strip.upcase
+        puts " "
+        if leagues.include? inp
+         games = find_games(inp)
+         game_titles = games.map { |game| game["title"] }
+         games_titles.each_with_index do |game_title, i|
+         end 
+        end
+      end
+
+
+  
+
         
       
-        
+       def find_games(inp)
+        @competitions.map { |competition| competition if competition["competition"]["name"].start_with?(inp) }.compact
+      end
          
 
 
