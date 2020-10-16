f5102dd085f71ec5b82cd2f7861a42888d93d745
igallardo89 <igallardo0716@gmail.com>
2020-10-16 10:52:09 -0400

    done

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,39 +16,41 @@ class CLI
         puts " "
         puts "Type a number listed to see Match Details or type 'exit' to quit"
         puts " "
-        puts " "
         inp = gets.strip.downcase
+
         while inp != 'exit' 
-          
-          if inp.to_i > 0
           choice = Games.find_by_index(inp.to_i-1)
-          if choice == nil
-            puts "Sorry try a Game from the list!"
-          else
+          if inp == nil
+            "Try another option or type 'exit' to quit" 
+          else inp == inp.to_i > 0
             puts " "
             puts "Name: #{choice.name}"
             puts "Date & Time: #{choice.date}"
+            puts " "
             puts "League: #{choice.league}"
+            puts " "
             puts "Team 1: #{choice.team_1}"
             puts "Team 2: #{choice.team_2}"
+            puts " "
             puts "Watch Game Highlights here: #{choice.url}!"
-          end
-
-        
-        
-     
-       
-          end  
+            puts " " 
+          end 
+          puts "Search for another team or type exit to 'exit'"
+          puts " "
+           inp = gets.strip.downcase
         end
+        puts " "
+        puts "Goodbye! See you next time!"
+        puts " "
       end
-
+    
 
     def print_games(match)
       puts " "
       puts "Game Matches:"
       puts " "
       match.each_with_index do | m , i|
-        puts "#{ i + 1}. #{m.name}"
+        puts "#{ i + 1 }. #{m.name}"
       end
     end
   end
