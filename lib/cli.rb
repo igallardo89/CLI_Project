84aef340e6196f8c608c77f562e1e72f20aacc21
igallardo89 <igallardo0716@gmail.com>
2020-10-16 14:09:51 -0400

    message

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,54 +3,54 @@
 #control flow of our program 
 
 class CLI
-    def start
-        puts " "
-        puts "Welcome to Game Highlights Finder!"
-        puts " "
-        puts " "
-        puts "Pick from the List of Games Below:"
-        puts " "
-        API.fetch_games(@game)
-        matches = Games.all
-        print_games(matches)
-        puts " "
-        puts "Type a number listed to see Match Details or type 'exit' to quit"
-        puts " "
-        inp = gets.strip.downcase
-
-        while inp != 'exit' 
-          choice = Games.find_by_index(inp.to_i-1)
-          if inp == nil
-            "Try another option or type 'exit' to quit" 
-          else inp == inp.to_i > 0
-            puts " "
-            puts "Name: #{choice.name}"
-            puts "Date & Time: #{choice.date}"
-            puts " "
-            puts "League: #{choice.league}"
-            puts " "
-            puts "Team 1: #{choice.team_1}"
-            puts "Team 2: #{choice.team_2}"
-            puts " "
-            puts "Watch Game Highlights here: #{choice.url}!"
-            puts " " 
-          end 
-          puts "Search for another team or type exit to 'exit'"
+  def start
+      puts " "
+      puts "Welcome to Game Highlights Finder!"
+      puts " "
+      puts " "
+      puts "Pick from the List of Games Below:"
+      puts " "
+      API.fetch_games(@game)
+      matches = Games.all
+      print_games(matches)
+      puts " "
+      puts "Type a number listed to see Match Details or type 'exit' to quit"
+      puts " "
+      inp = gets.strip.downcase
+      while inp != 'exit' 
+        choice = Games.find_by_index(inp.to_i-1)
+        if choice == nil
           puts " "
-           inp = gets.strip.downcase
-        end
+          puts "Invalid option:" 
+        else inp == inp.to_i > 0 && inp.to_i <= choice.length
+          puts " "
+          puts "Name: #{choice.name}"
+          puts "Date & Time: #{choice.date}"
+          puts " "
+          puts "League: #{choice.league}"
+          puts " "
+          puts "Team 1: #{choice.team_1}"
+          puts "Team 2: #{choice.team_2}"
+          puts " "
+          puts "Watch Game Highlights here: #{choice.url}!"
+          puts " " 
+        end 
+        puts "Search for another team or type exit to 'exit'."
         puts " "
-        puts "Goodbye! See you next time!"
-        puts " "
+         inp = gets.strip.downcase
       end
-    
+      puts " "
+      puts "Goodbye! See you next time!"
+      puts " "
+    end
+ 
 
-    def print_games(match)
-      puts " "
-      puts "Game Matches:"
-      puts " "
-      match.each_with_index do | m , i|
-        puts "#{ i + 1 }. #{m.name}"
-      end
+  def print_games(match)
+    puts " "
+    puts "Game Matches:"
+    puts " "
+    match.each_with_index do | m , i|
+      puts "#{ i + 1 }. #{m.name}"
     end
   end
+end
